1aa7dbb3e260b19ccefc4c5bb78525eddae5ff01
Ignacio <jcarrascomunguia@gmail.com>
2024-04-22 10:49:14 -0600

    Second commit

--- Curso0424/src/main/java/prueba/Selenium.java
+++ Curso0424/src/main/java/prueba/Selenium.java
@@ -8,7 +8,6 @@ public class Selenium {
 	public static void main(String[] args) {
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.google.com");
-		driver.get("https://www.facebook.com");
 	}
 
 }
